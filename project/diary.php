817391e4ff9977a8b3eb2d831187d704dd3f4c85
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2024-01-24 21:00:25 +0100

    created template of diary page

---
+++ project/diary.php
@@ -0,0 +1,17 @@
+<?php
+require_once("bootstrap.php");
+include("auth_session.php");
+
+$templateParams["title"] = "Diary";
+$templateParams["active"] = "diary.php";
+
+//Header settings
+$templateParams["headerLeftIcon"] = null; // null | notifications | back
+$templateParams["backPage"] = null; // null | file.php  -> the page to address when back is pressed
+$templateParams["headerRightIcon"] = "settings"; // null | search | settings
+
+//Footer setting
+$templateParams["footerActive"] = "diary"; // home | create | diary
+
+require("template/base.php");
+?>
