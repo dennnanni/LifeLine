df2fc7312c14dc2f4c091e58746076dcbeba60d3
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2024-01-25 23:17:25 +0100

    removed search button and added profile pics

--- project/js/search.js
+++ project/js/search.js
@@ -1,27 +1,27 @@
 $(document).ready(function() {
-    
-    let searchButton = document.querySelector("#searchButton");
+
     let searchBar = document.querySelector("#searchBar");
-    
-    searchButton.addEventListener("click", function() {
+
+    searchBar.addEventListener("input", function() {
         let data = searchBar.value;
-        console.log('boiade');
-        
-        request = $.ajax({
-            url: "utils/ajax.php",
-            type: "POST",
-            data: JSON.stringify(data),
-            success: function(response) {
-                showResult(response);
-            }
-        });
+        if (data == "") {
+            showResult("{}");
+        } else {
+            request = $.ajax({
+                url: "utils/ajax.php",
+                type: "POST",
+                data: JSON.stringify(data),
+                success: function(response) {
+                    showResult(response);
+                }
+            });
+        }
     });
-    
+
     function showResult(data) {
         let result = "";
         data = JSON.parse(data);
-        console.log(data);
-        
+
         for (let i = 0; i < data.length; i++) {
             let user = `
             <section class="w-100 ms-n40 ms-md-n3 mt-2">
@@ -31,7 +31,7 @@ $(document).ready(function() {
             `;
             result += user;
         }
-        
+
         let resultContainer = document.querySelector("#searchResult");
         resultContainer.innerHTML = result;
     }
--- project/template/search-form.php
+++ project/template/search-form.php
@@ -3,18 +3,14 @@
     <div class="ms-5 ms-md-6 pt-3 vh-100" name="lifeline">
     </div>
     <div class="flex-grow-1 text-nowrap w-100 pt-3">
-        <div class="input-group ms-n40">
+        <div class="ms-n40 me-40">
             <div class="form-outline">
-                <input type="search" id="searchBar" class="form-control rounded-0 rounded-start" placeholder="Search" />
+                <input type="search" id="searchBar" class="form-control rounded-5" placeholder="Search" />
                 <label class="form-label" for="searchBar" hidden>Search</label>
             </div>
-            <button type="button" class="btn btn-primary" id="searchButton">
-                <i class="fas fa-search"></i>
-            </button>
         </div>
         <div id="searchResult">
         </div>
-        
     </div>
 </div>
 <?php require("footer.php"); ?>
