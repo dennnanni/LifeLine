50c373cf6689a925b3a63c32551f6c547b4083d9
denise.nanni <denise.nanni1@gmail.com>
2024-01-25 12:03:35 +0100

    footer changed to stick to te bottom, inital searching page with vertical line

--- project/template/footer.php
+++ project/template/footer.php
@@ -1,31 +1,33 @@
-<footer class="mt-auto bg-light">
+<footer class="mt-auto bg-light fixed-bottom vw-100">
     <div class="row">
-        <div class="col-4">
-            <a class="nav-link flex-fill text-end" aria-current="page" href="home.php">
-            <?php if (isset($templateParams["footerActive"]) && $templateParams["footerActive"] == "home"): ?>
-                <i class="bi bi-house-fill h3"></i>
-            <?php else: ?>
-                <i class="bi bi-house h3"></i>
-            <?php endif; ?>
-            </a>
-        </div>
-        <div class="col-4">
-        <a class="nav-link flex-fill text-center" href="create.php">
-            <?php if (isset($templateParams["footerActive"]) && $templateParams["footerActive"] == "create"): ?>
-                <i class="bi bi-plus-circle-fill h3"></i>
-            <?php else: ?>
-                <i class="bi bi-plus-circle h3"></i>
-            <?php endif; ?>
-        </a>
-        </div>
-        <div class="col-4">
-            <a class="nav-link flex-fill text-start" href="diary.php">
-                <?php if (isset($templateParams["footerActive"]) && $templateParams["footerActive"] == "diary"): ?>
-                    <i class="bi bi-book-half h3"></i>
+        <div class="col-sm-12 col-12 d-flex">
+            <div class="col-4 col-sm-4">
+                <a class="nav-link flex-fill text-end" aria-current="page" href="home.php">
+                <?php if (isset($templateParams["footerActive"]) && $templateParams["footerActive"] == "home"): ?>
+                    <i class="bi bi-house-fill h3"></i>
                 <?php else: ?>
-                    <i class="bi bi-book h3"></i>
+                    <i class="bi bi-house h3"></i>
+                <?php endif; ?>
+                </a>
+            </div>
+            <div class="col-4 col-sm-4">
+            <a class="nav-link flex-fill text-center" href="create.php">
+                <?php if (isset($templateParams["footerActive"]) && $templateParams["footerActive"] == "create"): ?>
+                    <i class="bi bi-plus-circle-fill h3"></i>
+                <?php else: ?>
+                    <i class="bi bi-plus-circle h3"></i>
                 <?php endif; ?>
             </a>
+            </div>
+            <div class="col-4 col-sm-4">
+                <a class="nav-link flex-fill text-start" href="diary.php">
+                    <?php if (isset($templateParams["footerActive"]) && $templateParams["footerActive"] == "diary"): ?>
+                        <i class="bi bi-book-half h3"></i>
+                    <?php else: ?>
+                        <i class="bi bi-book h3"></i>
+                    <?php endif; ?>
+                </a>
+            </div>
         </div>
     </div>
 </footer>
--- project/template/search-form.php
+++ project/template/search-form.php
@@ -1,6 +1,16 @@
 <?php require("header.php"); ?>
-<?php foreach($templateParams["friends"] as $friend): ?>
-    <p><?php echo $friend["username"]?></p>
-    <p><?php echo $friend["name"]?></p>
-<?php endforeach?>
-<?php require("footer.php"); ?>
+<div class="d-flex vh-100 flex-column">
+    <div name="lifeline">
+        <?php foreach($templateParams["friends"] as $friend): ?>
+            <section class="d-inline-block">
+                <div class="ms-0 d-inline-block">
+                    <i class="fa-regular fa-user"></i>
+                </div>
+                <span class="d-inline-block"><?php echo $friend["username"]?></span>
+                <span class="d-inline-block"><?php echo $friend["name"]?></span>
+            </section>
+        <?php endforeach?>
+    </div>
+    
+</div>
+<?php require("footer.php"); ?> 
