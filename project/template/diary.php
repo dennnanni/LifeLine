be6055fb5544dc42216e279141d9ee6818c9633a
Denise Nanni - denise.nanni@studio.unibo.it <denise.nanni@studio.unibo.it>
2024-01-26 18:48:23 +0100

    updated layout of lifeline in search form, initial layout of diary

--- project/template/diary.php
+++ project/template/diary.php
@@ -1,2 +1,21 @@
 <?php require("header.php"); ?>
+
+<div class="d-flex vh-100">
+    <div class="ms-5 ms-md-6 mt-4 vh-100 overflow-visible" name="lifeline">
+        <div class="ms-n41 mt-n1 d-flex justify-content-center" name="diary-propic">
+            <img name="diary-propic" src="upload/default.jpg"/>
+        </div>
+    </div>
+    <div class="flex-grow-1 text-nowrap w-100 pt-3">
+        <div class="text-nowrap w-100 pt-3 ms-n40 me-40">
+            
+        </div>
+        <div class="text-nowrap w-100 pt-3">
+            
+            <div id="searchResult">
+            </div>
+        </div>
+    </div>
+</div>
+
 <?php require("footer.php"); ?>
--- project/template/search-form.php
+++ project/template/search-form.php
@@ -1,16 +1,19 @@
 <?php require("header.php"); ?>
-<div class="d-flex vh-100">
-    <div class="ms-5 ms-md-6 pt-3 h-100" name="lifeline">
-    </div>
-    <div class="flex-grow-1 text-nowrap w-100 pt-3">
-        <div class="ms-n40 me-40">
-            <div class="form-outline">
-                <input type="search" id="searchBar" class="form-control rounded-5" placeholder="Search" />
-                <label class="form-label" for="searchBar" hidden>Search</label>
+<div class="col-10 d-flex justify-content-center">
+    <div class="vh-100 position-relative">
+        <div class="ms-5 ms-md-6 ms-xl-6 pt-3 h-100 position-absolute" name="lifeline">
+        </div>
+        <div class="mx-40 w-100 pt-3 justify-content-center position-relative">
+            <div class="">
+                <div class="form-outline">
+                    <input type="search" id="searchBar" class="form-control rounded-5" placeholder="Search" />
+                    <label class="form-label" for="searchBar" hidden>Search</label>
+                </div>
+            </div>
+            <div class="position-relative" id="searchResult">
             </div>
         </div>
-        <div id="searchResult">
-        </div>
+        
     </div>
 </div>
 <?php require("footer.php"); ?>
