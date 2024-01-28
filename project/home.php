14db4ebe971386761b5722fc82c5418761953f2d
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2024-01-28 19:41:13 +0100

    filter automatic generation from db and added sport category

--- project/home.php
+++ project/home.php
@@ -5,6 +5,7 @@ include("auth_session.php");
 $_SESSION["current"] = "home";
 $templateParams["title"] = "Homepage";
 $templateParams["active"] = "home.php";
+$templateParams["js"] = "home.js";
 
 //Header settings
 $templateParams["headerLeftIcon"] = "notifications"; // null | notifications | back | logout
@@ -14,6 +15,7 @@ $templateParams["notificationsNumber"] = $dbh->getNewNotificationNumber($_SESSIO
 //Footer setting
 $templateParams["footerActive"] = "home"; // home | create | diary
 
+$templateParams["categories"] = $dbh->getAllCategories();
 $templateParams["posts"] = $dbh->loadHomePage($_SESSION["username"]);
 
 require("template/base.php");
--- project/template/home.php
+++ project/template/home.php
@@ -1,28 +1,9 @@
 <?php require("header.php"); ?>
 <div class="row w-100 ms-0 mt-2 px-2">
-    <div class="col-3 px-1 mb-2">
-        <button class="bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button">Food</button>
-    </div>
-    <div class="col-3 px-1 mb-2">
-        <button class="bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button">Fashion</button>
-    </div>
-    <div class="col-3 px-1 mb-2">
-        <button class="bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button">Sport</button>
-    </div>
-    <div class="col-3 px-1 mb-2">
-        <button class="bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button">Art</button>
-    </div>
-    <div class="col-3 px-1 mb-2">
-        <button class="bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button">Travel</button>
-    </div>
-    <div class="col-3 px-1 mb-2">
-        <button class="bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button">Love</button>
-    </div>
-    <div class="col-3 px-1 mb-2">
-        <button class="bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button">Fun</button>
-    </div>
-    <div class="col-3 px-1 mb-2">
-        <button class="bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button">Music</button>
-    </div>
+    <?php foreach($templateParams["categories"] as $category): ?>
+        <div class="col-3 px-1 mb-2">
+            <button id="<?php echo $category["name"]?>" class="bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button"><?php echo $category["name"] ?></button>
+        </div>
+    <?php endforeach; ?>
 </div>
 <?php require("footer.php"); ?>
