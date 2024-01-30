9c62e3f3f434aa980f8a508b8fe4dcfb1166c3f2
denise.nanni <denise.nanni1@gmail.com>
2024-01-30 13:46:14 +0100

    fixed layout in notifications page

--- project/template/notifications.php
+++ project/template/notifications.php
@@ -1,14 +1,25 @@
 <?php require("header.php"); ?>
-<div class="d-flex">
-    <div class="ms-5 ms-md-6 pt-3 vh-100" name="lifeline">
-    </div>
-    <div class="flex-grow-1 text-nowrap w-100 pt-3">
-        <?php foreach($templateParams["notifications"] as $notification):?>
-            <section class="w-100 ms-n40 ms-md-n3 mt-2">
-                <!-- <img name="search-propic" src="upload/<?php echo $notification["profilePic"]?>"/> -->
-                <span class="d-inline-block text-dark"><?php echo $notification["text"]?></span>
-            </section>
-        <?php endforeach;?>
+<div class="d-flex flex-grow-1 justify-content-center mb-7">
+    <div class="col-11 col-md-6 position-relative">
+        <div class="ms-7 pt-3 position-absolute h-100" id="lifeline">
+        </div>
+        <div class="w-100 pt-3 position-relative justify-content-center">
+            <div class="ms-4">
+                <?php foreach($templateParams["notifications"] as $notification):?>
+                    <div class="d-flex align-items-center ms-4 my-3">
+                        <div class="d-flex pe-3">
+                            <div name="icon-medium" class="d-flex justify-content-center align-items-center bg-secondary rounded-3">
+                                <i class="fa-solid <?php echo getNotificationIconClass($notification["type"]); ?>"></i>
+                            </div>
+                        </div>
+                        <section class="d-flex align-items-center">
+                            <!-- <img name="search-propic" src="upload/<?php echo $notification["profilePic"]?>"/> -->
+                            <span class="d-inline-block text-dark"><?php echo $notification["text"]?></span>
+                        </section>
+                    </div>
+                <?php endforeach;?>
+            </div>
+        </div>
     </div>
 </div>
 <?php require("footer.php"); ?>
