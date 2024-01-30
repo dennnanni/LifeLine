f143a213dcc4e90cb9bbb052f8428bf1c5d4a783
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2024-01-30 23:12:28 +0100

    filter maintain states even between more pages

--- project/template/home.php
+++ project/template/home.php
@@ -2,7 +2,9 @@
 <div class="row w-100 ms-0 mt-2 px-2">
     <?php foreach($templateParams["categories"] as $category): ?>
         <div class="col-3 px-1 mb-2">
-            <button id="<?php echo $category["name"]?>" class="filter bg-light border border-2 border-tertiary rounded-4 text-dark w-100 text-center" type="button"><?php echo $category["name"] ?></button>
+            <button id="<?php echo $category["name"]?>" class="filter border border-2 border-tertiary rounded-4 w-100 text-center <?php echo in_array($category["name"], $templateParams["selectedCategories"]) ? 'bg-primary text-light' : 'bg-light text-dark' ?>" type="button">
+                <?php echo $category["name"] ?>
+            </button>
         </div>
     <?php endforeach; ?>
 </div>
