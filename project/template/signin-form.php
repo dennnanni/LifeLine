8f0b24e1e43bc8a5af308d9314094f23344e535e
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2024-01-31 21:25:01 +0100

    removed onclick inline

--- project/template/signin-form.php
+++ project/template/signin-form.php
@@ -2,7 +2,7 @@
     <div id="toast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
             <strong class="me-auto text-secondary">Lifeline</strong>
-            <button id="toastDismiss" type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close" onclick="closeToast(this)"></button>
+            <button id="toastDismiss" type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body">
             <span id="errorMessage"></span>
