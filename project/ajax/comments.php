23eaf5b82b73b208fe0c7c572c2fd5fa95b14ef0
Nicolas Amadori <nicolas.amadori@studio.unibo.it>
2024-02-06 23:49:05 +0100

    added remove button in new comments

--- project/ajax/comments.php
+++ project/ajax/comments.php
@@ -5,6 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["comment"]) && isset($
     $dbh->createComment($_SESSION["username"], $_POST["postId"], $_POST["comment"]);
     $response["user"] = $dbh->getUser($_SESSION["username"]);
     $response["commentsCount"] = $dbh->getPost($_POST["postId"])["commentsCount"];
+    $response["commentId"] = $dbh->getLastCommentInPost($_POST["postId"])["id"];
     echo json_encode($response);
 }
 
--- project/js/comments.js
+++ project/js/comments.js
@@ -1,12 +1,12 @@
+let commentId = null;
+
 window.onload = function load() {
     let commentArea = document.getElementById("commentArea");
     let sendButton = document.getElementById("sendButton");
     let confirmDelete = document.getElementById("confirmDelete");
-    let deleteButton = document.getElementById("deleteButton");
     let toast = document.getElementById("toast");
-    let dismiss = document.getElementById("toastDismiss")
+    let dismiss = document.getElementById("toastDismiss");
     let postId = document.getElementById("postId").value;
-    let commentId = null;
 
     confirmDelete.addEventListener("click", function() {
         if (commentId == null) {
@@ -14,9 +14,9 @@ window.onload = function load() {
         }
 
         $.ajax({
-            url: "ajax/delete.php", 
-            type: "POST", 
-            data: {"commentId": commentId, "postId": postId}, 
+            url: "ajax/delete.php",
+            type: "POST",
+            data: { "commentId": commentId, "postId": postId },
             success: function() {
                 window.location.reload();
             }
@@ -56,23 +56,41 @@ function sendComment(comment, postId) {
         data: { "comment": comment, "postId": postId },
         success: function(data) {
             let response = JSON.parse(data);
-            addComment(response.user.username, response.user.profilePic, comment);
+            addComment(response.user.username, response.user.profilePic, comment, response.commentId);
         }
     });
 }
 
-function addComment(username, profilePic, text) {
+function addComment(username, profilePic, text, id) {
     let newComment = `
-    <div class="ms-4">
-        <a href="diary.php?username=${username}" class="text-decoration-none d-flex align-items-center">
-            <div class="rounded-circle propic-wrapper-sm">
-                <img class="propic" src="upload/${profilePic}" alt="friend profile picture"/>
+    <div class="ms-4 d-flex align-items-center">
+        <div class="rounded-circle propic-wrapper-sm">
+            <img class="propic" src="upload/${profilePic}" alt="friend profile picture"/>
+        </div>
+        <div class="d-inline-flexbox flex-grow-1 text-dark ms-2 mt-3">
+            <div class="d-flex justify-content-between">
+                <input type="hidden" value="${id}" />
+                <a href="diary.php?username=${username}" class="d-block text-dark fw-bold text-decoration-none d-flex align-items-center">
+                    ${username}
+                </a>
+                <label for="${id}" hidden>Delete the comment</label>
+                <button id="${id}" class="btn bg-light border-0" name="deleteButton">
+                    <span class="fa-solid fa-trash"></span>
+                </button>
             </div>
-            <div class="d-inline-block text-dark ms-2 w-100 mt-3">
-                <span class="fw-bold d-block">${username}</span>
-                <p class="text-break">${text}</p>
-            </div>
-        </a>
+            <p class="text-break">
+                ${text}
+            </p>
+        </div>
     </div>`;
     document.getElementById("comments").innerHTML += newComment;
+
+    let toast = document.getElementById("toast");
+    document.querySelectorAll('[name="deleteButton"]').forEach(function(button) {
+        button.addEventListener('click', function() {
+            toast.classList.add("show");
+            toast.parentElement.classList.add("display-overlapped");
+            commentId = button.parentElement.children[0].value;
+        });
+    })
 }
